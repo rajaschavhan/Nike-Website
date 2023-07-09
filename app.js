dc0e1179cebc74e2e71e09c65f052bc128ed9180
Rajas Chavhan <110460685+rajaschavhan@users.noreply.github.com>
2023-07-09 15:40:01 +0530

    Added all files

---
+++ app.js
@@ -0,0 +1,150 @@
+
+const wrapper=document.querySelector(".sliderWrapper");
+const menuItems=document.querySelectorAll(".menuItem");
+
+const products = [
+    {
+      id: 1,
+      title: "Air Force",
+      price: 119,
+      desc: "You've got the hops and the speed—lace up in shoes that enhance what you bring to the court. The latest AJ is all about take-offs and landings, with multiple Air units to get you off the ground. The upper is made with strong, reinforced leno-weave fabric that'll keep you contained and leave your game uncompromised.This low-top model is designed for playing on outdoor courts.",
+      colors: [
+        {
+          code: "black",
+          img: "./img/air.png",
+        },
+        {
+          code: "darkblue",
+          img: "./img/air2.png",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Air Jordan",
+      price: 149,
+      desc: "Inspired by the original that debuted in 1985, the Air Jordan 1 Low offers a clean, classic look that's familiar yet always fresh. With an iconic design that pairs perfectly with any 'fit, these kicks ensure you'll always be on point.",
+      colors: [
+        {
+          code: "lightgray",
+          img: "./img/jordan.png",
+        },
+        {
+          code: "green",
+          img: "./img/jordan2.png",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Blazer",
+      price: 109,
+      desc: "In the '70s, Nike was the new shoe on the block. So new in fact, we were still breaking into the basketball scene and testing prototypes on the feet of our local team. Of course, the design improved over the years, but the name stuck. The Nike Blazer Mid '77 Vintage—classic since the beginning.",
+      colors: [
+        {
+          code: "lightgray",
+          img: "./img/blazer.png",
+        },
+        {
+          code: "green",
+          img: "./img/blazer2.png",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Crater",
+      price: 129,
+      desc:"Helping the environment is now as easy as slipping on the Nike Crater Impact. Its translucent fabric on the toe reveals all the recycled material it is made from. While its deconstruction design gives it a one-of-a-kind, repurposed look. Made with soft Crater foam midsole, these shoes let you hit the street in style and total comfort. Nike Crater Impact features: Stitched web accents and a woven tongue label creates a unique look.",
+      colors: [
+        {
+          code: "black",
+          img: "./img/crater.png",
+        },
+        {
+          code: "lightgray",
+          img: "./img/crater2.png",
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Hippie",
+      price: 99,
+      desc:"Space Hippie is a story of trash transformed. From the upper to the outsole, at least 25% of the Space Hippie 04 is made from recycled material, by weight. Not only is it the most lightweight silhouette within the collection, it also has the lowest carbon footprint.The deconstructed design gives the look and feel of a prototype sent from the future with performance you have to feel to believe.",
+      colors: [
+        {
+          code: "gray",
+          img: "./img/hippie.png",
+        },
+        {
+          code: "black",
+          img: "./img/hippie2.png",
+        },
+      ],
+    },
+  ];
+
+  
+  let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+const currentProductDesc=document.querySelector(".productDesc");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+    currentProductDesc.textContent=choosenProduct.desc;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size,index) =>{
+
+    size.addEventListener("click", () =>{
+
+        currentProductSizes.forEach((size) =>{
+            size.style.backgroundColor="white";
+            size.style.color="black";
+        });
+
+        size.style.backgroundColor="black";
+        size.style.color="white";
+    });
+});
+
+
+const productButton=document.querySelector(".productButton");
+const payment=document.querySelector(".payment");
+const close=document.querySelector(".close");
+
+productButton.addEventListener("click",()=>{
+    payment.style.display="flex";
+});
+
+close.addEventListener("click",()=>{
+    payment.style.display="none";
+});
